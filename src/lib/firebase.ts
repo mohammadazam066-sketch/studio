a12fe4c79100065bc620de539713ca7a8ae25f48
Mohammad Azam <mohammadazam066@gmail.com>
2025-07-15 07:31:29 +0000

    // Import the functions you need from the SDKs you need import { initial

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -6,14 +6,13 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "REPLACE_WITH_YOUR_FIREBASE_API_KEY",
-  authDomain: "REPLACE_WITH_YOUR_FIREBASE_AUTH_DOMAIN",
-  projectId: "REPLACE_WITH_YOUR_FIREBASE_PROJECT_ID",
-  storageBucket: "REPLACE_WITH_YOUR_FIREBASE_STORAGE_BUCKET",
-  messagingSenderId: "REPLACE_WITH_YOUR_FIREBASE_MESSAGING_SENDER_ID",
-  appId: "REPLACE_WITH_YOUR_FIREBASE_APP_ID",
+  apiKey: "AIzaSyBrm6-aomerOz7DZlgSwmPFlU_FrhQj1m4",
+  authDomain: "tradeflow-tiu5k.firebaseapp.com",
+  projectId: "tradeflow-tiu5k",
+  storageBucket: "tradeflow-tiu5k.firebasestorage.app",
+  messagingSenderId: "28930351057",
+  appId: "1:28930351057:web:c67a6859f2ffdfd5da0af0"
 };
 
 // Initialize Firebase
