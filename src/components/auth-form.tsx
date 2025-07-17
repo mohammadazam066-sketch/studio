d237d659e0226fe0ae9eff9dc2ad505bbf48c6bd
Mohammad Azam <mohammadazam066@gmail.com>
2025-07-17 13:40:10 +0000

    check if homeowner login or registration working properly or not

--- src/components/auth-form.tsx
+++ src/components/auth-form.tsx
@@ -140,10 +140,10 @@ export function AuthForm({ mode, role }: AuthFormProps) {
                 {footerLinkText}
               </Link>
             </div>
-             { mode === 'login' && role === 'shop-owner' &&
+             { mode === 'login' && role === 'homeowner' &&
               <div className="text-sm text-muted-foreground text-center">
-                <span>Trying to log in as a Homeowner? </span>
-                <Link href={`/auth-pages/login?role=homeowner`} className="underline text-primary">
+                <span>Trying to log in as a Shop Owner? </span>
+                <Link href={`/auth-pages/login?role=shop-owner`} className="underline text-primary">
                   Click here
                 </Link>
               </div>
