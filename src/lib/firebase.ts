4e297c630c6d07bfaee38c740cd44f03aa528a98
Mohammad Azam <mohammadazam066@gmail.com>
2025-07-18 17:09:20 +0000

    const firebaseConfig = {   apiKey: "AIzaSyBrm6-aomerOz7DZlgSwmPFlU_FrhQj

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -10,11 +10,11 @@ import { getAuth, Auth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBrm6-aomerOz7DZlgSwmPFlU_FrhQj1m4",
-  authDomain: "tradelinkk.firebaseapp.com",
-  projectId: "tradelinkk",
-  storageBucket: "tradelinkk.appspot.com",
+  authDomain: "tradeflow-tiu5k.firebaseapp.com",
+  projectId: "tradeflow-tiu5k",
+  storageBucket: "tradeflow-tiu5k.appspot.com",
   messagingSenderId: "28930351057",
-  appId: "1:28930351057:web:c67a6859f2ffdfd5da0af0"
+  appId: "1:28930351057:web:6200a731ed264791da0af0"
 };
 
 let app: FirebaseApp;
