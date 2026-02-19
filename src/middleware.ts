9dcebbb89840968c0cb2de976b5b4ba06859fa06
Mohammad Azam <mohammadazam066@gmail.com>
2026-02-19 16:44:19 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,14 +1,6 @@
-import createMiddleware from 'next-intl/middleware';
- 
-export default createMiddleware({
-  // A list of all locales that are supported
-  locales: ['en', 'hi', 'kn'],
- 
-  // Used when no locale matches
-  defaultLocale: 'en'
-});
+
+export default function middleware() {}
  
 export const config = {
-  // Match only internationalized pathnames
-  matcher: ['/((?!api|_next|static|.*\\..*).*)']
+  matcher: []
 };
