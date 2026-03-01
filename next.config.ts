62556112d9bb3b25a2a955509c2610737c0501d1
Mohammad Azam <mohammadazam066@gmail.com>
2026-03-01 08:52:32 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,3 @@
-
-const withNextIntl = require('next-intl/plugin')();
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
@@ -34,4 +31,4 @@ const nextConfig = {
   },
 };
 
-module.exports = withNextIntl(nextConfig);
+module.exports = nextConfig;
